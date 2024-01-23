357134f7b04a70d4a7f56edd04ee54f745584320
ale <aa.olivera@hotmail.com>
2024-01-23 03:12:41 -0300

    net 4.8

--- ConsoleApp/Servicios/WebProvider.cs
+++ ConsoleApp/Servicios/WebProvider.cs
@@ -17,35 +17,11 @@ namespace Providers
     {
         private static readonly List<string> Proxys = new List<string> {
             //"http://localhost:63011/?url=",
-            "http://proxy2.gearhostpreview.com/?url=",
-            "http://proxy3.gearhostpreview.com/?url=",
-            "http://proxy4.gearhostpreview.com/?url=",
-            "http://proxy5.gearhostpreview.com/?url=",
-            "http://proxy6.gearhostpreview.com/?url=",
+           
 
-            "http://proxy17.gear.host/?url=",
-            "http://proxy18.gear.host/?url=",
-            "http://proxy19.gear.host/?url=",
-            "http://proxy20.gear.host/?url=",
-            "http://proxy21.gear.host/?url=",
-
-            "http://proxy22.gearhostpreview.com/?url=",
-            "http://proxy23.gearhostpreview.com/?url=",
-            "http://proxy24.gearhostpreview.com/?url=",
-            "http://proxy25.gearhostpreview.com/?url=",
-            "http://proxy26.gearhostpreview.com/?url=",
-
-            "http://proxy27.gear.host/?url=",
-            "http://proxy28.gear.host/?url=",
-            "http://proxy29.gear.host/?url=",
+        
             "http://proxy30.gear.host/?url=",
-            "http://proxy31.gear.host/?url=",
-
-            "http://proxy32.gearhostpreview.com/?url=",
-            "http://proxy33.gearhostpreview.com/?url=",
-            "http://proxy34.gearhostpreview.com/?url=",
-            "http://proxy35.gearhostpreview.com/?url=",
-            "http://proxy36.gearhostpreview.com/?url=",
+            "http://proxy31.gear.host/?url="
         };
 
         
